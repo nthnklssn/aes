88a1c16a2f69a6f7fb8b8826e57ed6d9d96d4016
Emraldis <alfie.feltham@gmail.com>
2019-03-06 23:32:59 -0500

    fixed errors

--- aes.c
+++ aes.c
@@ -1,31 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "aes.h"
 
-static void encryptionKeyExpansion (unit8_t *, unit8_t *);
-static void decryptionKeyExpansion (unit8_t *, unit8_t *);
+static void encryptionKeyExpansion (uint8_t **, uint8_t *);
+static void decryptionKeyExpansion (uint8_t **, uint8_t *);
 
 int main()
 {
-    uint8_t roundKeys[40][4];
-    roundKeys[0] = [0x00, 0x04, 0x08, 0x0c ];
-    roundKeys[1] = [0x01, 0x05, 0x09, 0x0d ];
-    roundKeys[2] = [0x02, 0x06, 0x0a, 0x0e ];
-    roundKeys[3] = [0x03, 0x07, 0x0b, 0x0f ];
-    uint8_t rawData = [0x00, 0x11, 0x22, 0x33,
-    0x44, 0x55, 0x66, 0x77,
-    0x88, 0x99, 0xaa, 0xbb,
-    0xcc, 0xdd, 0xee, 0xff];
+  uint8_t roundKeys[40][4] = {{ 0x00, 0x01, 0x02, 0x03 },
+  { 0x04, 0x05, 0x06, 0x07 },
+  { 0x08, 0x09, 0x0a, 0x0b },
+  { 0x0c, 0x0d, 0x0e, 0x0f }};
+  uint8_t rawData[4][4] =
+  {{0x00, 0x44, 0x88, 0xcc},
+  {0x11, 0x55, 0x99, 0xdd},
+  {0x22, 0x66, 0xaa, 0xee},
+  {0x33, 0x77, 0xbb, 0xff}};
+  shiftRows(rawData);
 
 
 
 
 }
 
-static void encryptionKeyExpansion (uint8_t * key, uint8_t * rowConstant){
+static void encryptionKeyExpansion (uint8_t ** key, uint8_t * rowConstant){
 
   uint8_t i;
-  key[5][0] ^= sbox[key[3][0]] ^ (*rc);
-  key[5][1] ^= sbox[key[3][1]];
-  key[5][2] ^= sbox[key[3][2]];
-  key[5][3] ^= sbox[key[3][3]];
+
+  key[5][0] ^= access_sbox(key[3][3]) ^ (*rowConstant);
+  key[5][1] ^= access_sbox(key[3][0]);
+  key[5][2] ^= access_sbox(key[3][1]);
+  key[5][3] ^= access_sbox(key[3][2]);
 }
