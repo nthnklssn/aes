784cce16226367bf30c8fe00dc2f931556e2cd3c
Emraldis <alfie.feltham@gmail.com>
2019-03-07 11:23:59 -0500

    Added functional key expansion (non-verified)

--- aes.c
+++ aes.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 #include "aes.h"
 
-static void initialize (keyStruct * , uint8_t *[4][4]);
-static void encryptionKeyExpansion (keyStruct *, uint8_t *);
-static void decryptionKeyExpansion (keyStruct *, uint8_t *);
+static void initialize (keyStruct * , uint8_t [10][4][4]);
+static void encryptionKeyExpansion (keyStruct *, uint8_t *, int);
+static void decryptionKeyExpansion (keyStruct *, uint8_t *, int);
+static void printRoundKey (keyStruct *);
 static void subBytes(uint8_t [4][4]);
 static void shiftRows(uint8_t [4][4]);
 
 int main()
 {
+  keyStruct * keys = malloc(sizeof(keyStruct));
   uint8_t roundKeys[10][4][4] = {{{ 0x00, 0x01, 0x02, 0x03 },
   { 0x04, 0x05, 0x06, 0x07 },
   { 0x08, 0x09, 0x0a, 0x0b },
@@ -57,10 +59,11 @@ int main()
   {0x22, 0x66, 0xaa, 0xee},
   {0x33, 0x77, 0xbb, 0xff}};
   shiftRows(rawData);
-
+  initialize(keys,roundKeys);
+  printRoundKey(keys);
 }
 
-static void initialize (keyStruct * keyStruct, uint8_t* key[4][4]){
+static void initialize (keyStruct * keyStruct, uint8_t key[10][4][4]){
 
   uint8_t rowConstant = 1;
   int i;
@@ -73,14 +76,21 @@ static void initialize (keyStruct * keyStruct, uint8_t* key[4][4]){
       }
     }
   }
-  encryptionKeyExpansion(keyStruct, &rowConstant);
+  encryptionKeyExpansion(keyStruct, &rowConstant, 1);
+
+  for(i=2;i<10;i++){
+    rowConstant *= 2;
+    encryptionKeyExpansion(keyStruct, &rowConstant, i);
+  }
 }
 
 static void subBytes(uint8_t data[4][4]){
 
-  for(int j=0;j<4;j++)
+int j,i;
+
+  for(j=0;j<4;j++)
   {
-    for(int i=0;i<4;i++)
+    for(i=0;i<4;i++)
     {
 
       printf("%02X\n", data[i][j]);
@@ -128,15 +138,33 @@ static void encryptionKeyExpansion (keyStruct * keys, uint8_t * rowConstant, int
 
   uint8_t i;
 
-  keyStruct->encryptKey[block][0][0] = keyStruct->encryptKey[block -1][1][0] ^ access_sbox(key[block -1][3][3]) ^ (*rowConstant);
-  keyStruct->encryptKey[block][0][1] = keyStruct->encryptKey[block -1][1][1] ^ access_sbox(key[block -1][3][0]);
-  keyStruct->encryptKey[block][0][2] = keyStruct->encryptKey[block -1][1][2] ^ access_sbox(key[block -1][3][1]);
-  keyStruct->encryptKey[block][0][3] = keyStruct->encryptKey[block -1][1][3] ^ access_sbox(key[block -1][3][2]);
+  keys->encryptKey[block][0][0] = keys->encryptKey[block -1][1][0] ^ access_sbox(keys->encryptKey[block -1][3][3]) ^ (*rowConstant);
+  keys->encryptKey[block][0][1] = keys->encryptKey[block -1][1][1] ^ access_sbox(keys->encryptKey[block -1][3][0]);
+  keys->encryptKey[block][0][2] = keys->encryptKey[block -1][1][2] ^ access_sbox(keys->encryptKey[block -1][3][1]);
+  keys->encryptKey[block][0][3] = keys->encryptKey[block -1][1][3] ^ access_sbox(keys->encryptKey[block -1][3][2]);
 
-  for (for i=1;i<4;i++){
-    keyStruct->encryptKey[block][i][0] = keyStruct->encryptKey[block][i-1][0] ^ keyStruct->encryptKey[block -1][i][0]
-    keyStruct->encryptKey[block][i][1] = keyStruct->encryptKey[block][i-1][1] ^ keyStruct->encryptKey[block -1][i][1]
-    keyStruct->encryptKey[block][i][2] = keyStruct->encryptKey[block][i-1][2] ^ keyStruct->encryptKey[block -1][i][2]
-    keyStruct->encryptKey[block][i][3] = keyStruct->encryptKey[block][i-1][3] ^ keyStruct->encryptKey[block -1][i][3]
+  for (i=1;i<4;i++){
+    keys->encryptKey[block][i][0] = keys->encryptKey[block][i-1][0] ^ keys->encryptKey[block -1][i][0];
+    keys->encryptKey[block][i][1] = keys->encryptKey[block][i-1][1] ^ keys->encryptKey[block -1][i][1];
+    keys->encryptKey[block][i][2] = keys->encryptKey[block][i-1][2] ^ keys->encryptKey[block -1][i][2];
+    keys->encryptKey[block][i][3] = keys->encryptKey[block][i-1][3] ^ keys->encryptKey[block -1][i][3];
+  }
+}
+
+static void printRoundKey (keyStruct * keys){
+  int i,j,k;
+
+  for(i=0;i<10;i++){
+    for(j=0;j<4;j++){
+      for(k=0;k<4;k++){
+        if(k != 3){
+          printf("%02X, ", keys->encryptKey[i][j][k]);
+        }else{
+          printf("%02X ", keys->encryptKey[i][j][k]);
+        }
+      }
+      printf("\n");
+    }
+    printf("\n");
   }
 }
