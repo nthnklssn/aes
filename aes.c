03cd6f31b70098e9f08d0fde77d33cab8cecfd0c
Nathan Klassen <nthnklssn@live.com>
2019-03-07 22:35:46 -0500

    Fixed mix columns operation

--- aes.c
+++ aes.c
@@ -53,21 +53,17 @@ void shiftRows(uint8_t data[4][4]){
 
 void mixColumns(uint8_t data[4][4])
 {
-printf("DATA WERE NEED\n" );
-  printf("%02X\n", mul2[data[0][0]] );
-  printf("%02X\n", mul3[data[1][0]] );
-  printf("%02X\n", data[0][0] );
-  printf("%02X\n", data[0][0] );
-
-for(int i = 0; i<4; i++)
-{
-  printf("%02X  %02X   %02X   %02X   %02X \n",   (mul2[(int)data[0][i]]), (mul3[(int)data[0][i]]), (data[0][i]), (data[0][i]),  ((data[0][i]) ^ (mul2[(int)data[0][i]]) ^ ((mul3[(int)data[0][i]]) ^ (data[0][i]))));
-  data[0][i] = ((mul2[data[0][i]]) ^ ((mul3[data[0][i]]) ^ (data[0][i])) ^ (data[0][i]) );
-  data[1][i] = ((data[1][i]) ^ (mul2[data[1][i]])) ^ ((mul3[data[1][i]]) ^ (data[1][i]));
-  data[2][i] = ((data[2][i]) ^ (data[2][i])) ^ ((mul2[data[2][i]]) ^ (mul3[data[2][i]]));
-  data[3][i] = ((mul3[data[3][i]]) ^ (data[3][i])) ^ ((data[3][i]) ^ (mul2[data[3][i]]));
-
-}
+  for(int i=0;i<4;i++)
+  {
+    uint8_t b0 = data[0][i];
+    uint8_t b1 = data[1][i];
+    uint8_t b2 = data[2][i];
+    uint8_t b3 = data[3][i];
+    data[0][i] = mul2[b0] ^ mul3[b1] ^ b2 ^ b3;
+    data[1][i] = b0 ^ mul2[b1] ^ mul3[b2] ^ b3;
+    data[2][i] = b0 ^ b1 ^ mul2[b2] ^ mul3[b3];
+    data[3][i] = mul3[b0] ^ b1 ^ b2 ^ mul2[b3];
+  }
 }
 
 int main()
@@ -79,52 +75,12 @@ int main()
     { 0x04, 0x05, 0x06, 0x07 },
     { 0x08, 0x09, 0x0a, 0x0b },
     { 0x0c, 0x0d, 0x0e, 0x0f }};
-  /*  uint8_t rawData[4][4] =
+   uint8_t rawData[4][4] =
     {{0x00, 0x44, 0x88, 0xcc},
     {0x11, 0x55, 0x99, 0xdd},
     {0x22, 0x66, 0xaa, 0xee},
     {0x33, 0x77, 0xbb, 0xff}};
 
-    //shiftRows(rawData);
-    //subBytes(rawData);*/
-   uint8_t rawData[4][4] =
-  {{0x19, 0xa0, 0x9a, 0xe9},
-  {0x3d, 0xf4, 0xc6, 0xf8},
-  {0xe3, 0xe2, 0x8d, 0x48},
-  {0xbe, 0x2b, 0x2a, 0x08}};
-  subBytes(rawData);
-  for(int j=0;j<4;j++)
-  {
-    printf("\n" );
-    for(int i=0;i<4;i++)
-    {
-
-      printf("%02X\n", rawData[i][j]);
-    }
-  }
-
-    shiftRows(rawData);
-
-      for(int j=0;j<4;j++)
-      {
-        printf("\n" );
-        for(int i=0;i<4;i++)
-        {
-
-          printf("%02X\n", rawData[i][j]);
-        }
-      }
-      mixColumns(rawData);
-
-        for(int j=0;j<4;j++)
-        {
-          printf("\n" );
-          for(int i=0;i<4;i++)
-          {
-
-            printf("%02X\n", rawData[i][j]);
-          }
-        }
 
 
 
