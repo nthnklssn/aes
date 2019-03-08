df72dc5b34152c5bc0ceaae0e4e7187fcb517fdb
Nathan Klassen <nthnklssn@live.com>
2019-03-07 21:54:22 -0500

    Added some initial changes with mix columns

--- aes.c
+++ aes.c
@@ -51,6 +51,25 @@ void shiftRows(uint8_t data[4][4]){
   data[3][3] = placeHolder[2];
 }
 
+void mixColumns(uint8_t data[4][4])
+{
+printf("DATA WERE NEED\n" );
+  printf("%02X\n", mul2[data[0][0]] );
+  printf("%02X\n", mul3[data[1][0]] );
+  printf("%02X\n", data[0][0] );
+  printf("%02X\n", data[0][0] );
+
+for(int i = 0; i<4; i++)
+{
+  printf("%02X  %02X   %02X   %02X   %02X \n",   (mul2[(int)data[0][i]]), (mul3[(int)data[0][i]]), (data[0][i]), (data[0][i]),  ((data[0][i]) ^ (mul2[(int)data[0][i]]) ^ ((mul3[(int)data[0][i]]) ^ (data[0][i]))));
+  data[0][i] = ((mul2[data[0][i]]) ^ ((mul3[data[0][i]]) ^ (data[0][i])) ^ (data[0][i]) );
+  data[1][i] = ((data[1][i]) ^ (mul2[data[1][i]])) ^ ((mul3[data[1][i]]) ^ (data[1][i]));
+  data[2][i] = ((data[2][i]) ^ (data[2][i])) ^ ((mul2[data[2][i]]) ^ (mul3[data[2][i]]));
+  data[3][i] = ((mul3[data[3][i]]) ^ (data[3][i])) ^ ((data[3][i]) ^ (mul2[data[3][i]]));
+
+}
+}
+
 int main()
 {
   /* Round keys and raw data are organized in a way where MSB is top left and LSB is bottom right.
@@ -60,13 +79,31 @@ int main()
     { 0x04, 0x05, 0x06, 0x07 },
     { 0x08, 0x09, 0x0a, 0x0b },
     { 0x0c, 0x0d, 0x0e, 0x0f }};
-    uint8_t rawData[4][4] =
+  /*  uint8_t rawData[4][4] =
     {{0x00, 0x44, 0x88, 0xcc},
     {0x11, 0x55, 0x99, 0xdd},
     {0x22, 0x66, 0xaa, 0xee},
     {0x33, 0x77, 0xbb, 0xff}};
+
+    //shiftRows(rawData);
+    //subBytes(rawData);*/
+   uint8_t rawData[4][4] =
+  {{0x19, 0xa0, 0x9a, 0xe9},
+  {0x3d, 0xf4, 0xc6, 0xf8},
+  {0xe3, 0xe2, 0x8d, 0x48},
+  {0xbe, 0x2b, 0x2a, 0x08}};
+  subBytes(rawData);
+  for(int j=0;j<4;j++)
+  {
+    printf("\n" );
+    for(int i=0;i<4;i++)
+    {
+
+      printf("%02X\n", rawData[i][j]);
+    }
+  }
+
     shiftRows(rawData);
-    //subBytes(rawData);
 
       for(int j=0;j<4;j++)
       {
@@ -77,6 +114,17 @@ int main()
           printf("%02X\n", rawData[i][j]);
         }
       }
+      mixColumns(rawData);
+
+        for(int j=0;j<4;j++)
+        {
+          printf("\n" );
+          for(int i=0;i<4;i++)
+          {
+
+            printf("%02X\n", rawData[i][j]);
+          }
+        }
 
 
 
